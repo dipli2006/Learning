169993c6c4667fbb1627d4186c07b0ea05d7342e
DivyaT <divinagar95@gmail.com>
2026-01-21 20:36:39 +0530

    Enhance practice.js with new functionalities
    Added functions to check divisibility, assign grades, and print even numbers.

--- javascript/practice.js
+++ javascript/practice.js
@@ -20,4 +20,37 @@ const profile={
 
 console.log(profile);
 console.log(typeof profile["followers"]);
+
 console.log(typeof profile["isFollowing"]);
+
+//3.get num from user and check multiple of 5
+let num=prompt("enter the number");
+if(num%5==0){
+    console.log("divisible by 5");
+}
+else{
+    console.log("not divisible");
+}
+
+//4.give grades according to their scores
+let score=prompt("enter your score");
+if(score>=80 && score<=100){
+    console.log("A grade");
+}
+else if(score>=70 && score<80){
+    console.log("B grade");
+}   
+else if(score>=60 && score<70){
+    console.log("C grade");
+}   
+else if(score>=50 && score<60){
+    console.log("D grade");
+}
+else if(score>=0 && score<50){
+    console.log("F grade");
+}
+
+//5.print all even numbers 0 to 20
+for(let i=2;i<=20;i+=2){
+    console.log(i);
+}
